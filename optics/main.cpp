2b98d37fe6175ffe47ad01a287f4ff0aad42e2b6
Lenny <valentin805@gmail.com>
2018-11-10 22:15:29 +0200

    Add app controller with output writing to .txt file

--- optics/main.cpp
+++ optics/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
-
+#include <string>
+#include <sstream>
+#include <fstream>
 using namespace std;
 
 class Optic{
@@ -16,6 +18,7 @@ public:
     Optic(string,string,string,double ,double);
     void showData();
     double getPrice();
+    string toFileLineString();
 };
 Optic::Optic(){}
 Optic::Optic(string opticType,string thickness,string material,double diopter,double price){
@@ -26,23 +29,34 @@ Optic::Optic(string opticType,string thickness,string material,double diopter,do
         this->price = price;
 }
 void Optic::showData(){
-        cout << '\t' << "Optic type: " << opticType << endl;
-        cout << '\t' << "thickness: " << thickness<< endl;
-        cout << '\t' << "material: " << material << endl;
-        cout << '\t' << "diopter: " << diopter << endl;
-        cout << '\t' << "price: " << price << endl;
-    }
+    cout << '\t' << "Optic type: " << opticType << endl;
+    cout << '\t' << "thickness: " << thickness<< endl;
+    cout << '\t' << "material: " << material << endl;
+    cout << '\t' << "diopter: " << diopter << endl;
+    cout << '\t' << "price: " << price << endl;
+}
+string Optic::toFileLineString(){
+    std::stringstream ss;
+    ss << opticType << endl;
+    ss << thickness << endl;
+    ss << material << endl;
+    ss << diopter << endl;
+    ss << price << endl;
+    return ss.str();
+}
 double Optic::getPrice(){
     return price;
 }
 
 class OpticFactory{
-private:
 public:
-    Optic CreateOptic();
+    OpticFactory();
+    Optic createOptic();
 };
 
-Optic OpticFactory::CreateOptic(){
+OpticFactory::OpticFactory(){}
+
+Optic OpticFactory::createOptic(){
         string opticType;
         cout << "Enter the type of Optic" << endl;
         cin >> opticType;
@@ -81,7 +95,24 @@ public:
     void showData();
     void addOptic(Optic newOptic);
     vector<Optic> getOptics();
+    string toFileLineString();
 };
+string Provider::toFileLineString(){
+    std::stringstream ss;
+    ss << bulstat << endl;
+    ss << fName << endl;
+    ss << lName << endl;
+    ss << phone << endl;
+    ss << address << endl;
+
+    ss << "-->" << endl;
+    for(unsigned int i = 0; i < optics.size(); i++){
+        ss << optics[i].toFileLineString();
+    }
+    ss << "<--" << endl;
+
+    return ss.str();
+}
 
 Provider::Provider(){ }
 Provider::Provider(string bulstat,string fName,string lName,string phone,string address){
@@ -144,12 +175,127 @@ private:
     string path;
 public:
     FileIO();
-    void saveProviders();
+    FileIO(string);
+    void saveProviders(vector<Provider>);
     vector<Provider> loadProviders();
 };
+FileIO::FileIO(){}
+
+FileIO::FileIO(string filePath){
+    path = filePath;
+}
+void FileIO::saveProviders(vector<Provider> providers){
+    ofstream outputStream;
+    outputStream.open(path);
+    for(unsigned int i = 0; i < providers.size(); i++){
+        string strProvider = providers[i].toFileLineString();
+        outputStream << strProvider;
+    }
+    outputStream << "TU-End" << endl;
+    outputStream.close();
+}
+
+class AppController{
+private:
+    OpticFactory opticFactory;
+    ProviderFactory providerFactory;
+    FileIO file;
+    vector<Provider> providers;
+public:
+    AppController(ProviderFactory, OpticFactory, FileIO);
+    void addProvider();
+    void addProvider(Provider);
+    void saveToFile();
+    void showProviders();
+};
+
+
+AppController::AppController(ProviderFactory p, OpticFactory o, FileIO f){
+    this->providerFactory = p;
+    this->opticFactory = o;
+    this->file = f;
+}
+
+void AppController::addProvider(){
+    Provider provider = providerFactory.createProvider();
+    providers.push_back(provider);
+}
+
+void AppController::addProvider(Provider provider){
+    providers.push_back(provider);
+}
+void AppController::showProviders(){
+    for(unsigned int i = 0; i < providers.size(); i++){
+        providers[i].showData();
+    }
+}
+void AppController::saveToFile(){
+    file.saveProviders(providers);
+}
+void showMenu(){
+    cout << "0. Exit." << endl;
+    cout << "1. Add Provider." << endl;
+    cout << "2. Show providers." << endl;
+    cout << "3. Save to File." << endl;
+}
 int main()
 {
     cout << "Welcome!" << endl;
+
+    // dummy data
+    ProviderFactory providerFactory;
+    OpticFactory opticFactory;
+    FileIO file("omegaData.txt");
+
+    AppController app(providerFactory,opticFactory, file);
+
+    Provider p1("bulstat1","fName1","lName1","phone1","address1");
+    Provider p2("bulstat2","fName2","lName2","phone2","address2");
+    Provider p3("bulstat3","fName3","lName3","phone3","address3");
+
+    Optic o1("pticType","thickness","material",0.5,10);
+    Optic o2("pticType2","thickness2","material2",1,20);
+    Optic o3("pticType3","thickness3","material3",2,30);
+    Optic o4("pticType4","thickness4","material4",3,40);
+    Optic o5("pticType5","thickness5","material5",4,50);
+
+    p1.addOptic(o1);
+    p1.addOptic(o2);
+    p1.addOptic(o5);
+    p3.addOptic(o3);
+    p2.addOptic(o4);
+
+    app.addProvider(p1);
+    app.addProvider(p2);
+    app.addProvider(p3);
+
+
+    //testting save to file
+
+    int choise = -1;
+    showMenu();
+    cin >> choise;
+    while(choise > 0){
+        if(choise == 1){
+            cout << "--> Add Provider" << endl;
+            cout << endl;
+            app.addProvider();
+        } else if(choise == 2){
+            cout << "--> Display" << endl;
+            cout << endl;
+            app.showProviders();
+        } else if(choise == 3){
+            cout << "--> Save to File" << endl;
+            cout << endl;
+            app.saveToFile();
+            cout << "Save complete" << endl;
+        }else {
+            break;
+        }
+        showMenu();
+        cin >> choise;
+    }
+
     cout << "Thank you for using us" << endl;
     return 0;
 }
