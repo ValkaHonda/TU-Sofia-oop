718b1a09fdaa7b2b93fd11ef977999fc827adad9
Lenny <valentin805@gmail.com>
2018-11-14 12:25:49 +0200

    private static members are directly visable to the nested class

--- InnerClass/main.cpp
+++ InnerClass/main.cpp
@@ -5,11 +5,17 @@ using namespace std;
 
 
 class Animal{
+private:
+    static int age;
 public:
-    int age;
     class Head{
     public:
         string name;
+        void cutOffHead(){
+            age++;
+            cout << age << endl;
+            cout << "The head is cut off " << endl;
+        }
     };
 
     void doSomething(){
@@ -18,14 +24,18 @@ public:
         cout << currentHead.name;
     }
 };
-
+int Animal::age = 10;
 
 int main()
 {
 
+
+
     Animal::Head h1;
     h1.name = "honda";
     cout << h1.name << endl;
+    cout << h1.name << endl;
+    h1.cutOffHead();
 
 
     return 0;
